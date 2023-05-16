b5febe64e558333320421af80eaf906e633ad3a5
Derek <derekdanielson84@gmail.com>
2023-05-16 10:29:57 -0600

    Notes added

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const logo = document.querySelector('img');
 
 const onClick = () => console.log('click event');
-
+const onDoubleClick = () => {
+    if (document.body.style.backgroundColor !== 'purple') {
+        document.body.style.backgroundColor = 'purple';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+};
 
 // Event Listeners
 logo.addEventListener('click', onClick);
